ed521cc95f7cebfba2d81d6099328c361382167f
SaeedGooda <saeedgooda219@gmail.com>
2021-07-24 21:55:06 +0200

    codeforces problem solving

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,19 @@
+/*
+    ==========================================
+    Problem Number : 50A
+    Problem Name   : Domino pilling
+    Author         : Saeed Gooda
+    ==========================================
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    cout << M*N / 2 << endl;
+    return 0;
+}
